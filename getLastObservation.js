f30e2bef3ea27b7f301f3d140a3a6383749975c1
Jan <jcz.kooijman@hetnet.nl>
2014-05-11 20:00:32 +0200

    Added functions manageRepetition(), setStartConditions(), setStopCondition(). Now you can choose interval of reading last measurements from the database and the number of last measurements to be read.

--- getLastObservation.js
+++ getLastObservation.js
@@ -1,6 +1,31 @@
 window.onload = function() {
-  document.getElementById("load").onclick = handleOnce;
+  document.getElementById("start").onclick = setStartConditions;
+  document.getElementById("stop").onclick = setStopCondition;
 };
+
+function setStartConditions() {
+  window.nr = 0;
+  window.setStop = false;
+  manageRepetition();
+}
+
+function setStopCondition() {
+  window.setStop = true;
+}
+
+function manageRepetition() {
+  var interval = document.getElementById("interval").value;
+  var number = document.getElementById("number").value;
+  handleOnce();
+  window.nr++;
+  if (window.nr < number && interval >= 1000 && window.setStop == false) {
+    myTimeout = window.setTimeout(manageRepetition, interval);
+  } else {
+    if (myTimeout != null) {
+      window.clearTimeout(myTimeout);
+    }
+  }
+}
 
 function getOnce(url, fn) {
   var ajax = new XMLHttpRequest();
